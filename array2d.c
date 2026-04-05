21b8fa793fb9bd803af0252404ee15a2a9de1e03
Hilmi <muhammad.hilmi.tif25@gmail.com>
2026-04-05 23:45:28 +0700

    update: nambah 2d non restricted 2d array

---
+++ array2d.c
@@ -0,0 +1,34 @@
+#include "array2d.h"
+
+#include <stdio.h>
+
+char text[MAX_BARIS][MAX_KOLOM];
+void initBuffer()
+{
+    for (int i = 0; i < MAX_BARIS; i++)
+    {
+        for (int j = 0; j < MAX_KOLOM; j++)
+        {
+            text[i][j] = '\0';
+        }
+    }
+}
+
+void insertChar(int baris, int kolom, char c)
+{
+    for (int i = MAX_KOLOM - 1; i > kolom; i--)
+    {
+        text[baris][i] = text[baris][i - 1];
+    }
+    text[baris][kolom] = c;
+}
+
+void deleteChar(int baris, int kolom)
+{
+    for (int i = kolom; i < MAX_KOLOM - 1; i++)
+    {
+        text[baris][i] = text[baris][i + 1];
+    }
+
+   text[baris][MAX_KOLOM - 1] = '\0';
+}
