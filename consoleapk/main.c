e36217cbd2f0ef539bb19db74edf513012b329cc
Hilmi <muhammad.hilmi.tif25@gmail.com>
2026-04-07 14:15:10 +0700

    update : nambahin display buffer

--- consoleapk/main.c
+++ consoleapk/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include "array2d.h"
+#include "boolean.h"
 
-// ===== FUNCTION DECLARATION =====
 int showMainMenu();
 int openFileMenu();
 void createNewFile();
 void searchFile();
 void exitProgram();
 
-// ===== FUNCTION IMPLEMENTATION =====
-
 // Menu utama
-int showMainMenu() {
+int showMainMenu()
+{
     int pilih;
     printf("\n=== MAIN MENU ===\n");
     printf("1. Open File\n");
@@ -21,10 +21,12 @@ int showMainMenu() {
 }
 
 // Sub menu Open File
-int openFileMenu() {
+int openFileMenu()
+{
     int pilih;
 
-    do {
+    do
+    {
         printf("\n=== OPEN FILE ===\n");
         printf("1. New File\n");
         printf("2. Search File\n");
@@ -32,59 +34,97 @@ int openFileMenu() {
         printf("Pilih: ");
         scanf("%d", &pilih);
 
-        switch(pilih) {
-            case 1:
-                createNewFile();
-                break;
-            case 2:
-                searchFile();
-                break;
-            case 3:
-                printf("Kembali ke menu utama...\n");
-                break;
-            default:
-                printf("Pilihan tidak valid!\n");
+        switch (pilih)
+        {
+        case 1:
+            createNewFile();
+            break;
+        case 2:
+            searchFile();
+            break;
+        case 3:
+            printf("Kembali ke menu utama...\n");
+            break;
+        default:
+            printf("Pilihan tidak valid!\n");
         }
 
-    } while(pilih != 3);
+    } while (pilih != 3);
 
     return 0;
 }
 
 // New file
-void createNewFile() {
-    printf("\n[Membuat file baru...]\n");
+
+void createNewFile()
+{
+    initBuffer();
+
+    int row = 0;
+    int col = 0;
+    char c;
+    getchar(); // buang ENTER sisa dari scanf
+    printf("Mulai mengetik (ENTER untuk baris baru, ESC untuk keluar)\n");
+
+    while (1)
+    {
+        c = getchar();
+
+        if (c == 27)
+        { // ESC
+            break;
+        }
+
+        if (c == '\n')
+        {
+            row++;
+            col = 0;
+        }
+        else
+        {
+            insertChar(row, col, c);
+            col++;
+        }
+    }
+
+    displayBuffer();
+    
 }
 
 // Search file
-void searchFile() {
+void searchFile()
+{
     printf("\n[Mencari file...]\n");
 }
 
 // Exit
-void exitProgram() {
+void exitProgram()
+{
     printf("\n[Keluar dari program]\n");
 }
 
-// mainnya 
-int main() {
+// mainnya
+int main()
+{
     int pilihan;
 
-    do {
+    do
+    {
         pilihan = showMainMenu();
 
-        switch(pilihan) {
-            case 1:
-                openFileMenu();
-                break;
-            case 2:
-                exitProgram();
-                break;
-            default:
-                printf("Pilihan tidak valid!\n");
+        switch (pilihan)
+        {
+        case 1:
+            openFileMenu();
+            break;
+        case 2:
+            exitProgram();
+            break;
+        default:
+            printf("Pilihan tidak valid!\n");
         }
 
-    } while(pilihan != 2);
+    } while (pilihan != 2);
 
     return 0;
 }
